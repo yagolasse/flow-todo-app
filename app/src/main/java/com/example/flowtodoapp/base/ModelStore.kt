90cb106e2b370d65e5f6a8ca2f50737272fd5cb4
Yago Lasse <yago.lasse@cubos.io>
2020-05-21 18:40:02 -0300

    update: almost finished refactor

--- app/src/main/java/com/example/flowtodoapp/base/ModelStore.kt
+++ app/src/main/java/com/example/flowtodoapp/base/ModelStore.kt
@@ -1,10 +1,57 @@
 package com.example.flowtodoapp.base
 
-import kotlinx.coroutines.flow.Flow
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.*
 
-interface ModelStore<S> {
+@FlowPreview
+@ExperimentalCoroutinesApi
+class ModelStore<A, S>(
+    private val reducer: Reducer<S, A>,
+    middlewareList: List<Middleware<A, S>>,
+    initialState: S
+) : ViewModel(), Store<A, S> {
 
-    fun process(intent: Intent<S>)
+    private val actions = Channel<A>()
+    private val store = MutableStateFlow(initialState)
 
-    fun modelState(): Flow<S>
+    init {
+        val actionFlow = actions
+            .receiveAsFlow()
+            .distinctUntilChanged()
+            .applyReducer()
+            .flowOn(Dispatchers.Main)
+
+        middlewareList
+            .bindReturningAction(actionFlow)
+            .asFlow()
+            .flattenMerge(middlewareList.size)
+            .feedbackToActions()
+            .flowOn(Dispatchers.Main)
+            .launchIn(viewModelScope)
+
+        actionFlow.launchIn(viewModelScope)
+    }
+
+    override fun process(action: A) {
+        actions.offer(action)
+    }
+
+    override fun storeState(): Flow<S> = store
+
+    private fun Flow<A>.applyReducer() = onEach {
+        store.value = reducer.reduce(store.value, it)
+    }
+
+    private fun List<Middleware<A, S>>.bindReturningAction(actionFlow: Flow<A>) = map {
+        it.bind(actionFlow, store)
+    }
+
+    private fun Flow<A>.feedbackToActions() = onEach {
+        actions.offer(it)
+    }
 }
